200f6df6de6112fe16139a0b05e38769e6539673
Colin O'Rourke <corourke@scharp.org>
2017-10-03 12:42:04 -0700

    isNearInteger description clarification

--- isNearInteger.R
+++ isNearInteger.R
@@ -1,4 +1,5 @@
-# This function will take a vector or scalar and test whether it is integer or not.
+# This function will take a vector or scalar and test whether it is full of
+# integer values or not.
 # Since often integer values are stored in double data types it will test that
 # a value is integer up to numerical precision (i.e., is near-integer).  In this
 # case, you might need to as.integer the value in order to use them (e.g., for
