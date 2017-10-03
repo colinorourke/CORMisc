717708924cf161673c1ef574d893f4a61618b368
Colin O'Rourke <corourke@scharp.org>
2017-10-03 11:42:58 -0700

    P-value Formats
    Initial commit of short function to format a P-value in a standard way.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 RProg
+
+This is a collection of short functions that I've found useful for analyzing data.
---
+++ fmtP.R
@@ -0,0 +1,46 @@
+fmtP = function(pval, strict=TRUE){
+  #pval must be supplied!
+  if(missing(pval))
+    stop("'pval' argument is required.")
+
+  #strict must be logical scalar.
+  if(!is.logical(strict) || length(strict) > 1)
+    stop("'strict' must be logical scalar value.")
+
+  #pval must be numeric vector
+  if(!is.numeric(pval)) stop("'pval' must be numeric.")
+
+  #which are missing?
+  pval_is_na = is.na(pval)
+
+  #just return pval (vector of NAs) if all are missing
+  if(all(pval_is_na)) {
+    warning("All 'pval' are NA.")
+    return(pval)
+  }
+
+  #is strict is TRUE make sure 0 <= pval[i] <= 1, for all i in 1 to length(pval).
+  #otherwise, just issue warning.  Not sure why one'd want the strickt
+  #option, but maybe useful in certain situations where you don't want
+  #code to error out?
+  #only check non-missing values
+  if(any(pval[!pval_is_na] < 0) || any(pval[!pval_is_na] > 1)){
+    if(strict)
+      stop("All 'pval' must be in interval [0,1].") else
+        warning("All 'pval' should in in interval [0,1].")
+  }
+
+
+  ret = sapply(pval, FUN = function(x){
+    x_rnd = round(x,3)
+    if(x_rnd <= 0)
+      "< 0.001" else
+        if(x_rnd < 0.1)
+          sprintf("%.3f",x_rnd) else
+            if(x_rnd < 0.99)
+              sprintf("%0.2f",x_rnd) else
+                "> 0.99"
+  })
+
+  ret
+}
